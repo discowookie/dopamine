81384a1e9222d5b3cf38f63de6a795aea0b9fa7f
Warren Craddock <warren.craddock@gmail.com>
2014-12-28 22:13:46 -0800

    Data blasted out on the USR LEDs. Looks good so far...

--- blinky.cc
+++ blinky.cc
@@ -10,6 +10,14 @@ int main(int argc, char** argv) {
   LED1642GW_Driver driver;
   printf("Initialized LED1642GW driver.\n");
 
+  for (int i = 0; i < 16; ++i) {
+    driver.brightness[i] = 0x0000;
+  }
+
+  driver.brightness[0] = 0xFFFF;
+  driver.brightness[1] = 0x0000;
+  driver.brightness[2] = 0xA5A5;
+  driver.brightness[3] = 0x5A5A;
   driver.write_all_brightness();
 
   return 0;
--- led1642gw_driver.h
+++ led1642gw_driver.h
@@ -14,30 +14,46 @@
 #define GPIO_DATAOUT      0x13C
 #define GPIO_DATAIN       0x138
 
+#define USR0              1<<21
 #define USR1              1<<22
+#define USR2              1<<23
+#define USR3              1<<24
+
 #define GPIO1_15          1<<15
+
+// #define SDI               1<<15
+// #define CLK               1<<16
+// #define LE                1<<17
+
+#define SDI               USR0
+#define CLK               USR1
+#define LE                USR2
 
 class LED1642GW_Driver {
 public:
   LED1642GW_Driver()
-  : clock_period(1000) {
-    mem_fd = open("/dev/mem", O_RDWR | O_SYNC);
+  : clock_period(250000) {
+    // Turn off the triggers for the USR0-3 LEDs, so we can use them for
+    // status.
+    system("echo none > /sys/class/leds/beaglebone\\:green\\:usr0/trigger");
+    system("echo none > /sys/class/leds/beaglebone\\:green\\:usr1/trigger");
+    system("echo none > /sys/class/leds/beaglebone\\:green\\:usr2/trigger");
+    system("echo none > /sys/class/leds/beaglebone\\:green\\:usr3/trigger");
 
+    // Open /dev/mem and memory map the GPIO registers.
+    mem_fd = open("/dev/mem", O_RDWR | O_SYNC);
     gpio1 =  (ulong*) mmap(
       NULL, GPIO_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, mem_fd, GPIO1_ADDR);
 
-    // Make GPIO1_15 an output by setting its output enable low.
-    gpio1[GPIO_OE/4] &= (0xFFFFFFFF ^ GPIO1_15);
+    // Make the three serial pins outputs by setting their output enables low.
+    gpio1[GPIO_OE/4] = 0xFFFFFFFF;
+    gpio1[GPIO_OE/4] ^=  SDI;
+    gpio1[GPIO_OE/4] ^=  CLK;
+    gpio1[GPIO_OE/4] ^=  LE;
 
-    for (int i = 0; i < 5; ++i) {
-      // Turn on GPIO1_15
-      gpio1[GPIO_DATAOUT/4] |= GPIO1_15;
-      sleep(1);
-
-      // Turn off GPIO1_15
-      gpio1[GPIO_DATAOUT/4] ^= GPIO1_15;
-      sleep(1);
-    }
+    gpio1[GPIO_DATAOUT/4] |= SDI;
+    gpio1[GPIO_DATAOUT/4] |= CLK;
+    gpio1[GPIO_DATAOUT/4] |= LE;
   }
 
   ~LED1642GW_Driver() {
@@ -47,14 +63,31 @@ public:
   void write_all_brightness() {
     for (int i = 0; i < 16; i++) {
       printf("Writing brightness value %d...\n", i);
-      write_brightness(brightness[i], false);
+      write_brightness(brightness[i], i == 15);
     }
   }
 
   void write_brightness(int data, bool global_latch) {
     for (int i = 15; i >= 0; i--) {
-      printf("  Writing bit %d...\n", i);
+      printf("  Writing bit %d, value is %d\n", i, data & (1 << i));
+
+      if (data & (1 << i)) {
+        gpio1[GPIO_DATAOUT/4] |= SDI;
+      } else {
+        gpio1[GPIO_DATAOUT/4] &= (0xFFFFFFFF ^ SDI);
+      }
+
+      if ((global_latch && i < 5) || (!global_latch && i < 3)) {
+        gpio1[GPIO_DATAOUT/4] |= LE;
+      }
+
+      gpio1[GPIO_DATAOUT/4] ^= CLK;
+      usleep(clock_period / 2);
+      gpio1[GPIO_DATAOUT/4] |= CLK;
+      usleep(clock_period / 2);
     }
+
+    gpio1[GPIO_DATAOUT/4] ^= LE;
   }
 
   int brightness[16];
