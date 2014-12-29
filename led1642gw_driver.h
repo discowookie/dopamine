5038fa073f0cafa4251b75a236853fa0d932851b
Warren Craddock <warren.craddock@gmail.com>
2014-12-28 19:56:06 -0800

    Moved stuff into the LED driver.

--- led1642gw_driver.h
+++ led1642gw_driver.h
@@ -3,15 +3,45 @@
 
 #include <stdio.h>
 
+#define GPIO0_ADDR        0x44E07000
+#define GPIO1_ADDR        0x4804C000
+#define GPIO2_ADDR        0x481AC000
+#define GPIO3_ADDR        0x481AF000
+
+#define GPIO_SIZE         (GPIO1_ADDR - GPIO0_ADDR)
+
+#define GPIO_OE           0x134
+#define GPIO_DATAOUT      0x13C
+#define GPIO_DATAIN       0x138
+
+#define USR1              1<<22
+#define GPIO1_15          1<<15
+
 class LED1642GW_Driver {
 public:
   LED1642GW_Driver()
   : clock_period(1000) {
+    mem_fd = open("/dev/mem", O_RDWR | O_SYNC);
 
+    gpio1 =  (ulong*) mmap(
+      NULL, GPIO_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, mem_fd, GPIO1_ADDR);
+
+    // Make GPIO1_15 an output by setting its output enable low.
+    gpio1[GPIO_OE/4] &= (0xFFFFFFFF ^ GPIO1_15);
+
+    for (int i = 0; i < 5; ++i) {
+      // Turn on GPIO1_15
+      gpio1[GPIO_DATAOUT/4] |= GPIO1_15;
+      sleep(1);
+
+      // Turn off GPIO1_15
+      gpio1[GPIO_DATAOUT/4] ^= GPIO1_15;
+      sleep(1);
+    }
   }
 
   ~LED1642GW_Driver() {
-
+    close(mem_fd);
   }
 
   void write_all_brightness() {
@@ -28,7 +58,11 @@ public:
   }
 
   int brightness[16];
+
   int clock_period;
+
+  int mem_fd;
+  ulong* gpio1;
 };
 
 #endif
